5d60b0b554c2618c8ffe0fd79cb60a119edbaac9
juanma06@gmail.com <juanma06@gmail.com@674f6c36-9023-6a0d-91d0-58138ae54c3b>
2014-06-21 03:26:54 +0000

    arregle un detalle porque no andaba.

--- trunk/ worms-server/trunk/worms-server/TPTaller/Juego.cpp
+++ trunk/ worms-server/trunk/worms-server/TPTaller/Juego.cpp
@@ -407,7 +407,7 @@ void Juego::checkColisionProyectil(structPaquete* paquete){
 				this->arma_actual = NULL;
 			}
 		}
-		if(this->getArmaActual()->getProyectil()->GetPosition().x > this->getEscalador()->getEscalaX() || this->getArmaActual()->getProyectil()->GetPosition().x < 0 ){
+		else if(this->getArmaActual()->getProyectil()->GetPosition().x > this->getEscalador()->getEscalaX() || this->getArmaActual()->getProyectil()->GetPosition().x < 0 ){
 			proj_in_air = false;
 			if (this->arma_actual->getTipo() != 6 ) this->mundo->destruir_cuerpo(arma_actual->getProyectil());
 			//this->arma_actual->aplicarExplosion(this->manejador);
